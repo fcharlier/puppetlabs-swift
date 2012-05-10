595421637741ad31b711c614f367c6757db2b207
François Charlier <francois.charlier@enovance.com>
2012-05-10 20:10:37 +0200

    Add the ratelimit middleware configuration

---
+++ manifests/proxy/ratelimit.pp
@@ -0,0 +1,30 @@
+#
+# Configure swift ratelimit.
+#
+# == Dependencies
+#
+# == Examples
+#
+# == Authors
+#
+#   François Charlier fcharlier@ploup.net
+#
+# == Copyright
+#
+# Copyright 2012 eNovance Labs licensing@enovance.com
+#
+class swift::proxy::ratelimit(
+  $clock_accuracy = 1000,
+  $max_sleep_time_seconds = 60,
+  $log_sleep_time_seconds = 0,
+  $rate_buffer_seconds = 5,
+  $account_ratelimit = 0
+) {
+
+  concat::fragment { 'swift_ratelimit':
+    target  => '/etc/swift/proxy-server.conf',
+    content => template('swift/proxy/ratelimit.conf.erb'),
+    order   => '26',
+  }
+
+}
